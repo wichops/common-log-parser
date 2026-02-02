789590cdcd0920fbdda9dbcd1be659aaf855f9cc
Luis Argumedo <huicho.argumedo@gmail.com>
2026-02-02 12:44:27 -0600

    feat: support for filename cli argument

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,33 @@
+use std::{fs::File};
+use std::io::{BufReader};
+use std::io::prelude::*;
+
+use clap::Parser;
+
+
 pub mod parser;
 
-fn main() {
-    println!("Hello, world!");
+use crate::parser::*;
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    filename: String,
+}
+
+fn main() -> Result<(), anyhow::Error> {
+    let args = Cli::parse();
+
+    println!("filename: {}", args.filename);
+    let file = File::open(args.filename)?;
+    let buf = BufReader::new(file);
+
+    for line in buf.lines() {
+        let line = line?;
+        let log_entry = parse_common_log(&line)?;
+
+        println!("{:?}", log_entry);
+    }
+
+    Ok(())
 }
